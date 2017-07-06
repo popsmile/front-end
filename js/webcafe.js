97694cce5b7ced8b40d6841fb6fc474d83a045c9
popsmile <popsmile7@gmail.com>
2017-07-06 16:20:01 +0900

    게시판 디자인 완성

--- js/webcafe.js
+++ js/webcafe.js
@@ -1,5 +1,5 @@
-// 서브메뉴 제어를 위한 스크립트
 $(function() {
+    // 메인메뉴 제어를 위한 스크립트
     $(".main-menu>li").hover(
         function() {
             $(this).children("ul").css("display", "block");
@@ -15,4 +15,8 @@ $(function() {
         function() {
             $(this).parent("ul").css("display", "none");
         });
+    //게시판 탭 메뉴 제어를 위한 스크립트
+    $(".notice-heading, .pds-heading").focus(function() {
+        $(this).parent().addClass("act").siblings().removeClass("act");
+    });
 });
