d65426463ec520076014d8329bed03c85c045439
popsmile <popsmile7@gmail.com>
2017-07-07 16:23:21 +0900

    인기사이트 디자인 완성

--- js/webcafe.js
+++ js/webcafe.js
@@ -19,4 +19,11 @@ $(function() {
     $(".notice-heading, .pds-heading").focus(function() {
         $(this).parent().addClass("act").siblings().removeClass("act");
     });
+    //관련사이트 키보드 포커스 시 클래스 추가 및 제거
+    $(".related-list a").focusin(function() {
+        $(this).parents(".related-list").addClass("list-focus-act");
+    });
+    $(".related-list a").focusout(function() {
+        $(this).parents(".related-list").removeClass("list-focus-act");
+    });
 });
